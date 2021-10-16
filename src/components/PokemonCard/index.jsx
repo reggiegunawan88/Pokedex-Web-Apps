6c6793c595820450b2361bc9a4937569b03c5579
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-17 00:02:32 +0700

    feat: onClick Pokemon card event (update thumbnail)

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react/cjs/react.development";
+import { PokemonContext } from "../PokemonList";
+
 function PokemonCard(props) {
+  const contextValue = useContext(PokemonContext);
+
+  const choosePokemon = () => {
+    contextValue.setPokemonThumbnail(props.data);
+    contextValue.setPokemonID(props.idx + 1);
+  };
+
   return (
-    <div className="flex-shrink-0 bg-blue-200 p-3 rounded mt-3 h-[fit-content] cursor-pointer">
-      <img className="w-20 h-20 mx-auto" alt="bulbasaur" src={props.data.image} />
+    <div onClick={choosePokemon} className="flex-shrink-0 bg-gray-50 p-3 rounded mt-3 h-[fit-content] cursor-pointer">
+      <img className="w-24 h-24 mx-auto" alt="bulbasaur" src={props.data.image} />
     </div>
   );
 }
--- src/components/PokemonCard/lazyLoading/index.jsx
+++ src/components/PokemonCard/lazyLoading/index.jsx
@@ -1,7 +1,7 @@
 function LazyLoading() {
   return (
     <div className="flex-shrink-0 bg-grey-40 animate-pulse p-3 rounded mt-3 h-[fit-content]">
-      <div className="w-20 h-20 mx-auto" />
+      <div className="w-24 h-24 mx-auto" />
     </div>
   );
 }
