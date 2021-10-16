adba2c5aa9843fc5c08d25859cf6986cf09d39f1
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-16 19:59:26 +0700

    config: setup Apollo GraphQL

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import Footer from "./components/Footer";
 import Home from "./pages/main/index.jsx";
 import PCP from "./pages/pcp/index.jsx";
 import PDP from "./pages/pdp/index.jsx";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Fragment } from "react";
+import { client } from "./components/GraphQL/setup";
+import { ApolloProvider } from "@apollo/client";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-blue-100">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Fragment>
-            <Header />
-            <Route path="/pokedex" component={PCP} />
-            <Route path="/pokemon" component={PDP} />
-          </Fragment>
-          <Footer />
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="min-h-screen bg-blue-100">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Fragment>
+              <Header />
+              <Route path="/pokedex" component={PCP} />
+              <Route path="/pokemon" component={PDP} />
+            </Fragment>
+            <Footer />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/GraphQL/queries.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+// all gql queries list, you can split this again after exceeding 300 lines of code
+export const LOAD_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      count
+      next
+      previous
+      status
+      message
+      results {
+        url
+        name
+        image
+      }
+    }
+  }
+`;
+
+export const POKEMON_DETAILS = gql`
+  query pokemon($name: String!) {
+    pokemon(name: $name) {
+      id
+      name
+      abilities {
+        ability {
+          name
+        }
+      }
+      moves {
+        move {
+          name
+        }
+      }
+      types {
+        type {
+          name
+        }
+      }
+      message
+      status
+    }
+  }
+`;
