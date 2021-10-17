244c0a886379d142472fe9e033c2308aa16f0e63
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-17 22:55:24 +0700

    fix: useContext import and firebase pr.yml

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react/cjs/react.development";
+import React, { useContext } from "react";
 import { PokemonContext } from "../PokemonList";
 
 function PokemonCard(props) {
