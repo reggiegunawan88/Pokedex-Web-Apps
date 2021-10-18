b90f550fd40a3ec657f1e51fc34514634350584a
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-18 23:45:47 +0700

    feat: fade animation for modal popup

--- src/components/PokemonDetails/components/modal/index.jsx
+++ src/components/PokemonDetails/components/modal/index.jsx
@@ -1,24 +1,29 @@
+import React, { useContext } from "react";
+import { userInteractionContext } from "components/PokemonDetails";
+import "./index.css";
+
 function Modal() {
+  const status = useContext(userInteractionContext);
   return (
     <div>
-      {/* modal dark bg */}
-      <div className="fixed bg-gray-500 top-0 bottom-0 right-0 left-0 content-none z-50 opacity-75"></div>
-      {/* modal content */}
-      <div className="absolute max-w-sm w-full z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col bg-white mx-4 px-4 py-9 text-center rounded-lg">
-          <div className="">
-            <p className="font-sans text-xl font-bold">
-              Nice Catch! <br /> Give your Pokemon a nickname.
-            </p>
+      {status.modal && (
+        <div className="fade-in">
+          <div className="fixed bg-gray-500 top-0 bottom-0 right-0 left-0 content-none z-50 opacity-75"></div>
+          <div className="fade-in absolute max-w-sm w-full z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className="flex flex-col bg-white mx-4 px-4 py-9 text-center rounded-lg">
+              <p className="font-sans text-xl font-bold">
+                Nice Catch! <br /> Give your Pokemon a nickname.
+              </p>
+              <div className="mt-6">
+                <input className="w-full h-10 border-default border-grey-30 p-3 outline-none" placeholder="Pokemon nickname" />
+              </div>
+              <button className="bg-redBtn w-full py-3 mt-10 rounded-full">
+                <p className="font-minecraft text-lg text-white font-semibold">SAVE</p>
+              </button>
+            </div>
           </div>
-          <div className="mt-6">
-            <input className="w-full h-10 border-default border-grey-30 p-3 outline-none" placeholder="Pokemon nickname" />
-          </div>
-          <button className="bg-redBtn w-full py-3 mt-10 rounded-full">
-            <p className="font-minecraft text-lg text-white font-semibold">SAVE</p>
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
