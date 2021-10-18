a17a98ab47860372cdfd7857b763299920efd3a9
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-18 17:38:20 +0700

    feat: modal popup pokemon nickname UI component

---
+++ src/components/PokemonDetails/catch-button/index.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { PokemonDetailsContext } from "../index";
+
+function CatchButton() {
+  const ctxValue = useContext(PokemonDetailsContext);
+  const catchPokemon = () => {
+    const chance = Math.random();
+    if (chance < 0.5) {
+      console.log("failed");
+    } else {
+      console.log("success");
+      console.log("pokemon:" + ctxValue.pokemon.name);
+    }
+  };
+  return (
+    <button className="transform transition duration-300 hover:scale-125" onClick={catchPokemon}>
+      <img alt="pokeball-catch" src="/assets/pokeball-catch.png" className="mx-auto" />
+      <p className="font-minecraft text-xl font-bold text-redBtn">CATCH</p>
+    </button>
+  );
+}
+
+export default CatchButton;
