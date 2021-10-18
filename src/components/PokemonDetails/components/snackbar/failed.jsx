356ef854929e88953ed039d983b38cec5fc3f56b
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-19 02:22:55 +0700

    feat: create snackbar success and failed UI and animation

--- src/components/PokemonDetails/components/snackbar/failed.jsx
+++ src/components/PokemonDetails/components/snackbar/failed.jsx
@@ -1,14 +1,22 @@
+import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { userInteractionContext } from "components/PokemonDetails";
+import "./index.css";
 
 function SnackbarFailed() {
+  const status = useContext(userInteractionContext);
   return (
-    <div className="fixed flex w-full justify-center z-50">
-      <div className="max-w-md bg-snackbarRed rounded-lg mx-4">
-        <div className="flex flex-row gap-x-4 px-6 py-3 text-white items-center">
-          <FontAwesomeIcon icon="times" />
-          <p className="font-sans font-bold">Pokemon escaped! Try again.</p>
+    <div>
+      {status.snackbarFailed && (
+        <div className="slide fixed flex w-full justify-center z-50">
+          <div className="max-w-md bg-snackbarRed rounded-lg mx-4">
+            <div className="flex flex-row gap-x-4 px-6 py-3 text-white items-center">
+              <FontAwesomeIcon icon="times" className="cursor-pointer" onClick={status.closeSnackbarFailed} />
+              <p className="font-sans font-bold">Pokemon escaped! Try again.</p>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/PokemonDetails/components/snackbar/success.jsx
+++ src/components/PokemonDetails/components/snackbar/success.jsx
@@ -1,14 +1,22 @@
+import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { userInteractionContext } from "components/PokemonDetails";
+import "./index.css";
 
 function SnackbarSuccess() {
+  const status = useContext(userInteractionContext);
   return (
-    <div className="fixed flex w-full justify-center z-50">
-      <div className="max-w-sm bg-snackbarGreen rounded-lg mx-4">
-        <div className="flex flex-row gap-x-4 px-6 py-3 text-white items-center">
-          <FontAwesomeIcon icon="check" />
-          <p className="font-sans font-bold">Your pokemon has added to your inventory.</p>
+    <div>
+      {status.snackbarSuccess && (
+        <div className="slide fixed flex w-full justify-center z-50">
+          <div className="max-w-md bg-snackbarGreen rounded-lg mx-4">
+            <div className="flex flex-row gap-x-4 px-6 py-3 text-white items-center">
+              <FontAwesomeIcon icon="check" className="cursor-pointer" onClick={status.closeSnackbarSuccess} />
+              <p className="font-sans font-bold">Your pokemon has added to your inventory.</p>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
