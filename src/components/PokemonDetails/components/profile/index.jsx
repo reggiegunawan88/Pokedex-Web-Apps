6d3903dcbb567483947480b9738071656d6bce5d
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-20 09:50:49 +0700

    fix: skeleton loading on pokemon detail

--- src/components/PokemonDetails/components/profile/index.jsx
+++ src/components/PokemonDetails/components/profile/index.jsx
@@ -34,7 +34,7 @@ function PokemonProfile() {
   };
   const renderImg = () => {
     if (pokemon.length === 0) {
-      return <div className="w-37 h-37 rounded skeleton animate-pulse  tablet:w-50 tablet:h-50 tablet:justify-self-center  tablet:mt-12"></div>;
+      return <div className="w-37 h-37 rounded bg-skeleton animate-pulse tablet:w-50 tablet:h-50 tablet:justify-self-center tablet:mt-12"></div>;
     } else {
       return <img className="w-37 tablet:w-50 justify-self-center" alt={pokemon.name} src={pokemon.sprites.front_default} />;
     }
@@ -44,7 +44,11 @@ function PokemonProfile() {
     <div className="grid grid-cols-2 tablet:grid-cols-1">
       {/* pokemon name, types */}
       <div className="flex flex-col text-left place-self-center tablet:gap-y-2 tablet:text-center">
-        {!ctxValue.loading ? <p className="font-bold font-minecraft text-2xl tablet:text-3xl capitalize">{pokemon.name}</p> : <div className="h-5 w-full rounded skeleton animate-pulse"></div>}
+        {!ctxValue.loading ? (
+          <p className="font-bold font-minecraft text-2xl tablet:text-3xl capitalize">{pokemon.name}</p>
+        ) : (
+          <div className="bg-skeleton h-5 w-full rounded skeleton animate-pulse"></div>
+        )}
         <div className="flex flex-row gap-x-3 mt-2 justify-center">{renderTypes()}</div>
       </div>
       {renderImg()}
