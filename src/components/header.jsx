99cd82cbfbb9e7ee8d9874bf4f69e132b32015ff
reggiegunawan <reggiegunawan88@gmail.com>
2021-10-20 16:25:58 +0700

    feat: header icon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,21 +6,21 @@ function Header() {
 
   return (
     <div className="fixed top-0 w-full py-4 bg-pokedexBlue text-center z-50">
-      <div className="flex flex-row items-center mx-5 justify-between text-white">
+      <div className="flex flex-row items-center mx-6 justify-between text-white">
         {/* back button */}
         <div className="flex flex-row items-center cursor-pointer" onClick={() => history.goBack()}>
           <FontAwesomeIcon icon="arrow-left" />
           <p className="font-bold font-minecraft mx-4 hidden laptop:block">Back</p>
         </div>
-        <div className="mx-auto">
+        <div className="flex">
           <Link to="/pokedex">
             <img alt="pokeball-logo" src="/assets/pokedex-pixel.png" className="h-9 tablet:h-12" />
           </Link>
         </div>
         <div className="flex flex-row items-center">
           <Link to="/my-pokemon">
-            {/* <img className="h-7 w-7" alt="pokeball-icon" src="/assets/pokeball.png" /> */}
-            <p className="font-bold font-minecraft mx-4 hidden laptop:block">My Pokemon</p>
+            {/* <img className="h-6  tablet:h-10 tablet:w-10" alt="pokeball-icon" src="/assets/pokeball-pixel.png" /> */}
+            <p className="font-bold font-minecraft">My Pokemon</p>
           </Link>
         </div>
       </div>
